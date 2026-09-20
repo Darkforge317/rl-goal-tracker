3bc14af8f651576fc2f5c7b3d56998d9ce2ec00c
Darkforge317 <Pierre.Gravelle1@gmail.com>
2026-09-20 13:22:27 -0600

    Add "New Update" Header

--- src/main/java/com/darkforge317/goaltracker/ui/ChangelogPanel.java
+++ src/main/java/com/darkforge317/goaltracker/ui/ChangelogPanel.java
@@ -1,8 +1,11 @@
 package com.darkforge317.goaltracker.ui;
 
+import com.darkforge317.goaltracker.services.ChangelogService;
 import net.runelite.client.ui.ColorScheme;
+import net.runelite.client.ui.FontManager;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
@@ -10,7 +13,8 @@ import java.awt.GridLayout;
 
 public class ChangelogPanel extends JPanel
 {
-    public ChangelogPanel(Runnable onSeeAll, Runnable onClose)
+    public ChangelogPanel(ChangelogService.ChangelogEntry entry, boolean isNewUpdate,
+                          Runnable onSeeAll, Runnable onClose)
     {
         super(new BorderLayout());
         setBackground(ColorScheme.DARK_GRAY_COLOR);
@@ -27,6 +31,24 @@ public class ChangelogPanel extends JPanel
         headerBar.add(seeAllButton);
         headerBar.add(closeButton);
 
+        JLabel titleLabel = new JLabel(isNewUpdate ? "New Update!" : "Goal Tracker Changelog");
+        titleLabel.setFont(FontManager.getRunescapeBoldFont());
+        titleLabel.setForeground(ColorScheme.BRAND_ORANGE);
+        titleLabel.setAlignmentX(java.awt.Component.CENTER_ALIGNMENT);
+
+        JLabel versionLabel = new JLabel("V" + entry.getVersion() + " Changes");
+        versionLabel.setForeground(ColorScheme.LIGHT_GRAY_COLOR);
+        versionLabel.setAlignmentX(java.awt.Component.CENTER_ALIGNMENT);
+
+        JPanel titleSection = new JPanel();
+        titleSection.setLayout(new javax.swing.BoxLayout(titleSection, javax.swing.BoxLayout.Y_AXIS));
+        titleSection.setBackground(ColorScheme.DARK_GRAY_COLOR);
+        titleSection.setBorder(new EmptyBorder(8, 8, 8, 8));
+        titleSection.add(titleLabel);
+        titleSection.add(javax.swing.Box.createVerticalStrut(4));
+        titleSection.add(versionLabel);
+
         add(headerBar, BorderLayout.NORTH);
+        add(titleSection, BorderLayout.CENTER);
     }
 }
